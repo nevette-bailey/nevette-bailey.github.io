6ead562f3f69f6dda26f2ac249cc1a683ba39c63
Nevette Bailey <nevettebailey@gmail.com>
2020-09-05 01:52:54 -0400

    Updates

---
+++ precache-manifest.8e010f94359ef7835a94b5f5858d8283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542052f0dead01df57eed86328223f30",
+    "url": "/nevette-bailey.github.io/index.html"
+  },
+  {
+    "revision": "a58bf624dde01777722c",
+    "url": "/nevette-bailey.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d74cc995743072f44f2",
+    "url": "/nevette-bailey.github.io/static/js/2.4ddfe278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nevette-bailey.github.io/static/js/2.4ddfe278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58bf624dde01777722c",
+    "url": "/nevette-bailey.github.io/static/js/main.8ab7ded2.chunk.js"
+  },
+  {
+    "revision": "131a654c02429b266849",
+    "url": "/nevette-bailey.github.io/static/js/runtime-main.6e7f8c74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nevette-bailey.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
